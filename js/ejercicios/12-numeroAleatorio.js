693ff66f049431ec401756817cf95a08b65671d8
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-03-23 23:55:42 -0300

    ejercicio 12 tp2

---
+++ js/ejercicios/12-numeroAleatorio.js
@@ -0,0 +1,3 @@
+const numeroAleatorio = Math.floor(Math.random() * 99) + 1;
+console.log(`Tu número aleatorio es: ${numeroAleatorio}`);
+document.write(`<h3>Número generado: ${numeroAleatorio}</h3>`);
