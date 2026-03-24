c77fde7c4bfd6bf6d29e871b96a84d88c388a317
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-03-24 12:31:46 -0300

    Actualización: Mejora lógica ejercicio 16

--- js/ejercicios/16-textoreves.js
+++ js/ejercicios/16-textoreves.js
@@ -1,8 +1,22 @@
-const textoOriginal = prompt("Ingresá un texto:");
-if (textoOriginal) {
+const textoOriginal = prompt("Ingresá un texto para darlo vuelta:");
+if (textoOriginal !== null) {
+    let textoInvertido = "";
+
+    // Empezamos en la última letra (largo - 1)
+    // Mientras i sea mayor o igual a 0
+    // Restamos 1 a i en cada vuelta (i--)
+    for (let i = textoOriginal.length - 1; i >= 0; i--) {
+        textoInvertido += textoOriginal.charAt(i);
+    }
+    alert("Texto invertido: " + textoInvertido);
+    console.log(`Original: ${textoOriginal} | Invertido: ${textoInvertido}`);
+}
+//con array
+//const textoOriginal = prompt("Ingresá un texto:");
+//if (textoOriginal) {
   // split('') -> Convierte "HOLA" en ['H','O','L','A']
   // reverse() -> Da vuelta el array: ['A','L','O','H']
   // join('')  -> Une todo de nuevo: "ALOH"
-  const invertido = textoOriginal.split("").reverse().join("");
-  alert("Invertido: " + invertido);
-}
+  //const invertido = textoOriginal.split("").reverse().join("");
+  //alert("Invertido: " + invertido);
+//}
