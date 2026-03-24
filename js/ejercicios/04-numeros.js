22d51ead9a5a477f478f9e4785965631992b5a43
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-03-23 21:21:13 -0300

    ejercicio 4 tp2 con notas

--- js/ejercicios/04-numeros.js
+++ js/ejercicios/04-numeros.js
@@ -1,22 +1,16 @@
 let suma = 0;
-
+//va ingresando numeros hasta que hace click en cancelar
 while (true) {
-    let entrada = prompt("Ingrese un número (Cancelar para salir):");
-
-    // Si el usuario presiona Cancelar
+  let entrada = prompt("Ingrese un número (Cancelar para salir):");
     if (entrada === null) {
-        break;
-    }
-
-    let numero = Number(entrada);
-
-    // Validar si es un número
-    if (isNaN(numero)) {
-        alert("No es un número válido");
-        continue;
-    }
-
-    suma += numero;
+    break;
+  }
+  let numero = Number(entrada);
+  // Validar si es un número
+  if (isNaN(numero)) { //necesita un valor para validarlo
+    alert("No es un número válido");
+    continue;
+  }
+  suma += numero;
 }
-
 alert("La suma total es: " + suma);
