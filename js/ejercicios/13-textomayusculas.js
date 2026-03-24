d6b272f0d46ef29efbbe29c933e99235fad45358
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-03-24 11:21:29 -0300

    ejercicio 14 tp2

--- js/ejercicios/13-textomayusculas.js
+++ js/ejercicios/13-textomayusculas.js
@@ -1,11 +1,13 @@
-const textoUsuario = prompt("Inngresá el texto que quieras convertir en mayusculas:");
-if (textoUsuario !== null) { //Validamos que el usuario no haya presionado "Cancelar" (null)
-   const textoMayusculas = textoUsuario.toUpperCase(); // con toUpperCase() transformamos el texto
-   console.log("Original: " + textoUsuario);
-   console.log("Transformado: " + textoMayusculas);
-   alert("Tu texto en mayúsculas es:\n" + textoMayusculas);
-    document.write("<h3>Resultado: " + textoMayusculas + "</h3>");
-
+const textoUsuario = prompt(
+  "Ingresá el texto que quieras convertir en mayusculas:",
+);
+if (textoUsuario !== null) {
+  //Validamos que el usuario no haya presionado "Cancelar" (null)
+  const textoMayusculas = textoUsuario.toUpperCase(); // con toUpperCase() transformamos el texto
+  console.log("Original: " + textoUsuario);
+  console.log("Transformado: " + textoMayusculas);
+  alert("Tu texto en mayúsculas es:\n" + textoMayusculas);
+  document.writeln("<h3>Resultado: " + textoMayusculas + "</h3>");
 } else {
-    alert("No ingresaste ningún texto.");
+  alert("No ingresaste ningún texto.");
 }
